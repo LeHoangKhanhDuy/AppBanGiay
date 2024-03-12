dd44f1b1b38a4fc17a839032c38ea1e641f2e7d9
KhanhDuy <lhkhanhduy.dev@gmail.com>
2024-03-12 13:57:56 +0700

    First commit

---
+++ app_ban_giay/lib/features/authentication/screens/signup/verify_email.dart
@@ -0,0 +1,28 @@
+import 'package:app_ban_giay/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SingleChildScrollView(
+
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              //Image
+
+              //Title
+
+              //Btn
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
